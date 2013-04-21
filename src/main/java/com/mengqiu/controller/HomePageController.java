fda85260e3a7bf801e1ea18fa3be915f130f26a4
NANA <pengmengqiu803@gmail.com>
2013-04-21 15:37:41 +0800

    fix

--- src/main/java/com/mengqiu/controller/HomePageController.java
+++ src/main/java/com/mengqiu/controller/HomePageController.java
@@ -1,10 +1,12 @@
 package com.mengqiu.controller;
 
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 public class HomePageController {
 
     @RequestMapping(value="/home", method = RequestMethod.GET)
